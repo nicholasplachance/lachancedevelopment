b13de34a7f431b71e3dc92d21ed3ede34ac8dc74
Nicholas LaChance <nicholasplachance@gmail.com>
2021-07-28 15:48:20 -0500

    Introuduce react-router to project and begin adding simple routing

--- src/App.js
+++ src/App.js
@@ -1,74 +1,49 @@
-import './App.css';
-import 'aos/dist/aos.css';
+import "./App.css";
+import "aos/dist/aos.css";
 
-import { useEffect, useState } from 'react';
+import React, { useState } from "react";
+import { Route, Switch, withRouter } from "react-router";
 
-import Aos from 'aos';
-import BackDrop from './components/BackDrop/BackDrop';
-import Container from './components/Container/Container';
-import Footer from './components/Footer/Footer';
-import HeroBanner from './components/HeroBanner/HeroBanner';
-import NavBar from './components/NavBar/NavBar';
-import React from 'react';
-import Section from './components/Sections/Section';
-import SideDrawer from './components/SideDrawer/SideDrawer';
+import BackDrop from "./components/BackDrop/BackDrop";
+import Footer from "./components/Footer/Footer";
+import HomePage from "./pages/homepage/homepage.component";
+import NavBar from "./components/NavBar/NavBar";
+import SideDrawer from "./components/SideDrawer/SideDrawer";
+import WorkShowCasePage from "./pages/work-showcase/work-showcase.component";
 
 function App() {
-	const [sideDrawerOpen, setSideDrawerOpen] = useState(false);
+  const [sideDrawerOpen, setSideDrawerOpen] = useState(false);
 
-	const drawerToggleClickHandler = () => {
-		setSideDrawerOpen((prevState) => {
-			return { sideDrawerOpen: !prevState.setSideDrawerOpen };
-		});
-	};
+  const drawerToggleClickHandler = () => {
+    setSideDrawerOpen((prevState) => {
+      return { sideDrawerOpen: !prevState.setSideDrawerOpen };
+    });
+  };
 
-	const backDropClickHandler = () => {
-		setSideDrawerOpen(false);
-	};
+  const backDropClickHandler = () => {
+    setSideDrawerOpen(false);
+  };
 
-	let sideDrawer;
-	let backdrop;
+  let sideDrawer;
+  let backdrop;
 
-	if (sideDrawerOpen) {
-		sideDrawer = <SideDrawer />;
-		backdrop = <BackDrop click={backDropClickHandler} />;
-	}
-	return (
-		<div className='App'>
-			<NavBar drawerToggleClickHandler={drawerToggleClickHandler} />
-			<SideDrawer show={sideDrawerOpen} />
-			{backdrop}
-			<HeroBanner />
-			<main className='spacer'></main>
-			<Container>
-				<Section
-					title={'LaChance Development'}
-					subtitle={'Established 2021.'}
-					services={[
-						'- Freelance Web Developer',
-						'',
-						'- Design and build custom websites and web apps',
-						'',
-						'- Completely custom code and layout',
-						'',
-						'- Mobile friendly responsive design',
-						'',
-						'- SEO Optimized'
-					]}
-					address={'St. Louis, Missouri'}
-				/>
-				<Section
-					id='contact'
-					title={'About'}
-					services={[
-						'Hi, my name is Nicholas LaChance, one of my greatest passions is technology. With my passion I taught myself many technologies that allow me to create websites and web applications. My goal is to provide mobile responsive and SEO friendly websites written with clean code.'
-					]}
-				/>
-				<Section id='contact' title={'Contact info'} services={['Email:', 'nicholasplachance@gmail.com']} />
-			</Container>
-			<Footer />
-		</div>
-	);
+  if (sideDrawerOpen) {
+    sideDrawer = <SideDrawer />;
+    backdrop = <BackDrop click={backDropClickHandler} />;
+  }
+  return (
+    <div className='App'>
+      <NavBar drawerToggleClickHandler={drawerToggleClickHandler} />
+      <SideDrawer show={sideDrawerOpen} />
+      {backdrop}
+      <Switch>
+        <Route path='/work' component={withRouter(WorkShowCasePage)} />
+        <Route exact path='/' component={HomePage} />
+      </Switch>
+
+      <Footer />
+    </div>
+  );
 }
 
-export default App;
+export default withRouter(App);
---
+++ src/pages/work-showcase/work-showcase.component.jsx
@@ -0,0 +1,5 @@
+const WorkShowCasePage = () => {
+  return <div className='show-case-page'>Work Show Case</div>;
+};
+
+export default WorkShowCasePage;
